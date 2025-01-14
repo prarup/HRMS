f276bfc98857f16cc8faaf8a043fcdc896f56a1f
prarup <prarup12@gmail.com>
2025-01-14 20:59:59 +0545

    Company Screen

--- lib/companyscreen.dart
+++ lib/companyscreen.dart
@@ -1,4 +1,3 @@
-// company_screen.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
@@ -70,12 +69,16 @@ class _CompanyScreenState extends State<CompanyScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Enter Company')),
-      body: Padding(
+      appBar: AppBar(title: const Text(''),backgroundColor: Color(0xFFFFFFFF),),
+      body: Container(
+        color: Colors.white,  // Set the body color to white
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+
           children: [
+            Image.asset('assets/applogo.png', height: 100),
+            const SizedBox(height: 25),
             TextField(
               controller: _companyController,
               decoration: InputDecoration(
@@ -83,15 +86,23 @@ class _CompanyScreenState extends State<CompanyScreen> {
                 filled: true,
                 fillColor: Colors.grey[200],
                 enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(color: Colors.grey[400]!),
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(color: Color(0xFFFFFFFF)!),
                 ),
                 focusedBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10),
                   borderSide: BorderSide(color: Colors.blue),
                 ),
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(10.0), // Adjust padding as needed
+                  child: Image.asset(
+                    'assets/company.png', // Replace with your asset icon path
+                    height: 20, // Adjust size as needed
+                  ),
+                ),
               ),
             ),
+
             if (_errorMessage != null)
               Padding(
                 padding: const EdgeInsets.only(top: 8.0),
@@ -102,10 +113,45 @@ class _CompanyScreenState extends State<CompanyScreen> {
               ),
             const SizedBox(height: 20),
             ElevatedButton(
+
               onPressed: _next,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12), // Adjust padding to make the button smaller
+              ),
               child: _isLoading
                   ? CircularProgressIndicator(color: Colors.white)
-                  : Text('Next'),
+                  : Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Next',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        color: Colors.grey[200],
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Powered by Dots and Dashes',
+              style: TextStyle(
+                color: Colors.grey[700],
+                fontWeight: FontWeight.normal,
+              ),
             ),
           ],
         ),
