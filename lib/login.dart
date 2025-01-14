5c6742d755d2223612f10d3fd25937db8c3cb342
acharya152 <pranavachaerya152@gmail.com>
2025-01-14 13:12:00 +0545

    Modified the login page UI

--- lib/login.dart
+++ lib/login.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'employee_detail_page.dart';  // Correct path to your EmployeeDetailPage
+import 'employee_detail_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -18,16 +18,19 @@ class _LoginPageState extends State<LoginPage> {
   bool _isLoading = false;
   bool _obscurePassword = true;
 
-  final _formKey = GlobalKey<FormState>(); // Form key to validate inputs
+  final _formKey = GlobalKey<FormState>();
+  String? _errorMessage;
 
   void _login() async {
-    if (!_formKey.currentState!.validate()) {
-      return; // If form is not valid, exit
+    if (_usernameController.text.isEmpty || _passwordController.text.isEmpty) {
+      _showError('Username/Password Is Required.');
+      return;
     }
 
     final url = Uri.parse('https://88collection.dndts.net/api/method/frappe.val.api.login');
     setState(() {
       _isLoading = true;
+      _errorMessage = null;
     });
 
     try {
@@ -45,7 +48,6 @@ class _LoginPageState extends State<LoginPage> {
           final token = data['auth']['token'];
           await _storage.write(key: 'auth_token', value: token);
 
-          // Navigate to EmployeeDetailPage after login success
           Navigator.pushReplacement(
             context,
             MaterialPageRoute(builder: (context) => const EmployeeDetailPage()),
@@ -57,7 +59,7 @@ class _LoginPageState extends State<LoginPage> {
         _showError('Login Failed. Please check your credentials.');
       }
     } catch (e) {
-      _showError('Login Failed. Please check your credentials.');
+      _showError('An error occurred. Please try again later.');
     } finally {
       setState(() {
         _isLoading = false;
@@ -66,168 +68,168 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   void _showError(String message) {
-    // Create a custom overlay to show the message at the top
-    OverlayEntry overlayEntry = OverlayEntry(
-      builder: (context) => Positioned(
-        top: MediaQuery.of(context).padding.top + 10, // Position it just below the status bar
-        left: 20,
-        right: 20,
-        child: Material(
-          color: Colors.transparent,
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 251, 69, 69),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Text(
-              message,
-              style: const TextStyle(color: Colors.white, fontSize: 16),
-            ),
-          ),
-        ),
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        style: const TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.orange,
+      behavior: SnackBarBehavior.floating,
+      duration: const Duration(seconds: 2),
+      margin: const EdgeInsets.all(16),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
       ),
     );
 
-    // Show the overlay
-    Overlay.of(context).insert(overlayEntry);
-
-    // Remove the overlay after a delay
-    Future.delayed(const Duration(seconds: 3), () {
-      overlayEntry.remove();
-    });
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // Set background to white
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // Centering content vertically
-          children: [
-            // Main content in the center
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Form(
-                key: _formKey, // Form widget for validation
-                child: Column(
-                  children: [
-                    Image.asset('assets/applogo.png', height: 100),
-
-                    const SizedBox(height: 30),
-
-                    const Text(
-                      'Login to HRMS!',
-                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 5),
-                    const Text(
-                      'Welcome',
-                      style: TextStyle(fontSize: 16, color: Colors.grey),
-                    ),
-                    const SizedBox(height: 30),
-
-                    // Username field
-                    TextFormField(
-                      controller: _usernameController,
-                      decoration: InputDecoration(
-                        prefixIcon: const Icon(Icons.person),
-                        hintText: 'Username',
-                        hintStyle: TextStyle(color: Colors.black.withOpacity(0.5)),  // Placeholder color
-                        filled: true,
-                        fillColor: Colors.grey[200],
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(color: Colors.grey[400]!),
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        Image.asset('assets/applogo.png', height: 100),
+                        const SizedBox(height: 30),
+                        const Text(
+                          'Login to HRMS!',
+                          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                         ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(color: Colors.blue),
+                        const SizedBox(height: 5),
+                        const Text(
+                          'Welcome',
+                          style: TextStyle(fontSize: 16, color: Colors.grey),
                         ),
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Username is required';
-                        }
-                        return null;
-                      },
-                      cursorColor: Colors.black,
-                    ),
-                    const SizedBox(height: 16),
-
-                    // Password field
-                    TextFormField(
-                      controller: _passwordController,
-                      obscureText: _obscurePassword,
-                      decoration: InputDecoration(
-                        prefixIcon: const Icon(Icons.lock),
-                        hintText: 'Password',
-                        hintStyle: TextStyle(color: Colors.black.withOpacity(0.5)),  // Placeholder color
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _obscurePassword
-                                ? Icons.visibility_off
-                                : Icons.visibility,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              _obscurePassword = !_obscurePassword;
-                            });
-                          },
-                        ),
-                        filled: true,
-                        fillColor: Colors.grey[200],
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(color: Colors.grey[400]!),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(color: Colors.blue),
-                        ),
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Password is required';
-                        }
-                        return null;
-                      },
-                      cursorColor: Colors.black,
-                    ),
-                    const SizedBox(height: 20),
-
-                    // Sign-in button or loading indicator
-                    _isLoading
-                        ? Center( // Center the loading indicator within its parent
-                            child: CircularProgressIndicator(
-                              valueColor: AlwaysStoppedAnimation<Color>(Colors.blue), // Custom color
-                              strokeWidth: 4, // Adjust stroke width to make it more prominent
+                        const SizedBox(height: 30),
+                        TextField(
+                          controller: _usernameController,
+                          decoration: InputDecoration(
+                            prefixIcon: const Icon(Icons.person),
+                            hintText: 'Username',
+                            filled: true,
+                            fillColor: Colors.grey[200],
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Colors.grey[400]!),
                             ),
-                          )
-                        : ElevatedButton(
-                            onPressed: _login,
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.blue,
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 60,
-                                vertical: 14,
-                              ),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                            ),
-                            child: const Text(
-                              'Login',
-                              style: TextStyle(color: Colors.white, fontSize: 16),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Colors.blue),
                             ),
                           ),
-                  ],
+                        ),
+                        const SizedBox(height: 16),
+                        TextField(
+                          controller: _passwordController,
+                          obscureText: _obscurePassword,
+                          decoration: InputDecoration(
+                            prefixIcon: const Icon(Icons.lock),
+                            hintText: 'Password',
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _obscurePassword
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _obscurePassword = !_obscurePassword;
+                                });
+                              },
+                            ),
+                            filled: true,
+                            fillColor: Colors.grey[200],
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Colors.grey[400]!),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Colors.blue),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        if (_errorMessage != null)
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 8.0),
+                            child: Text(
+                              _errorMessage!,
+                              style: const TextStyle(color: Colors.red),
+                            ),
+                          ),
+
+                        ElevatedButton(
+                          onPressed: _login,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 60,
+                              vertical: 14,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          if (_isLoading)
+            Container(
+              color: Colors.white.withOpacity(0.8), // Semi-transparent background
+              child: Center(
+                child: Container(
+                  width: 100, // Width of the white box
+                  padding: const EdgeInsets.all(16), // Padding inside the box
+                  decoration: BoxDecoration(
+                    color: Colors.white, // White background for the box
+                    borderRadius: BorderRadius.circular(12), // Rounded corners
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        blurRadius: 10, // Subtle shadow for the box
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.blue), // Blue loading bar
+                        strokeWidth: 4,
+                      ),
+                      SizedBox(height: 16), // Spacing between loader and text
+
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
-        ),
+            )
+          ,
+        ],
       ),
     );
   }
